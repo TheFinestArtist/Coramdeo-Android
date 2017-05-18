b7b169781121947d2e9cd9e495da7410decdc528
Wonhee David Lee <wonheedavid@wonheedavid-macbookpro2.roam.corp.google.com>
2017-05-18 17:54:36 +0900

    First commit

---
+++ app/src/main/java/com/ecoramdeo/MainActivity.java
@@ -0,0 +1,33 @@
+package com.ecoramdeo;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
+
+import butterknife.BindView;
+import butterknife.ButterKnife;
+
+public class MainActivity extends AppCompatActivity {
+
+    @BindView(R.id.web_view)
+    WebView webView;
+    @BindView(R.id.progress_bar)
+    ProgressBar progressBar;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+
+        webView.loadUrl("http://ecoramdeo.com/");
+        webView.setWebViewClient(new WebViewClient() {
+            public void onPageFinished(WebView view, String url) {
+                progressBar.setVisibility(View.GONE);
+            }
+        });
+    }
+}
